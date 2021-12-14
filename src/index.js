953508c629dc8d200a66462e19920e7182edd6af
Leandro ADAGBE <67783416+Leonelberio@users.noreply.github.com>
2021-12-14 21:06:26 +0000

    First part of Js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,72 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//InnerHTML
+document.getElementById("greeting").innerHTML = "Hello";
+
+//document.write()
+document.write("Hello world");
+
+//window.alert()
+//alert("Attention, jaime les cookies");
+
+// console.log()
+console.log("Moi c'est madame Concon");
+
+//Variables
+var x;
+let y;
+let z;
+
+//Utiliser les variables
+x = 7;
+y = 10;
+z = 6 + y;
+
+let p = x + 11;
+
+x = 8;
+console.log(x);
+console.log(z);
+console.log(p);
+
+//Constantes
+const w = 10;
+
+//Types de variables
+//Integer
+let phoneNumber = 22222222;
+
+//Float
+let netWeight = 10.5;
+
+//Boolean
+let isBeautiful = true;
+let isRaining = false;
+var hasTerribleLifeInTogo = true;
+
+//String
+let nameOfTheGirl = "Vicky";
+
+let _car_name = "";
+
+//Les operateurs
+//Les operateurs arithmétiques
+//+ addition
+//- soustraction
+//* multiplication
+// / division
+// ** Exposant => 10 ** 2
+// % Modulo (Reste de la division)
+
+let modulo = 10 % 5;
+console.log(modulo);
+
+//++ Incrementation
+let i = 12;
+i++; // i =i +1 ;
+console.log(i);
+
+//Decrementation
+let h = 10;
+h--;
+console(h);
