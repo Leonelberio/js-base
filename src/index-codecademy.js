142a6a32f8348767e816840177b09c0bb8ae0880
Leandro ADAGBE <67783416+Leonelberio@users.noreply.github.com>
2021-12-14 22:17:46 +0000

    index-codecademy

---
+++ src/index-codecademy.js
@@ -0,0 +1,20 @@
+//Methods => Retourne des informations a propos d'un objet
+// console.log(Math.random());
+
+//Library(objet) => Ce qui vient avant le .
+//Methods => Ce qui vient après le .
+
+//Numbers
+let amount = 6;
+let price = 4.99;
+
+//String
+let message = "good night";
+console.log(message.length);
+
+//Booleans
+
+//Prompt
+let date = prompt("Veuillez saisir votre date de naissance : ");
+let age = 2021 - date;
+console.log("Fou mal guerri!!!Vous avez " + age + " ans");
